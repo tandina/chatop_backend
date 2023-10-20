ac9599587bccbbeac247002a0b1e0beb00dd1e16
tandina <touretmc@protonmail.com>
2023-10-20 23:51:53 +0200

    implementation de la sécurité ok, ajout de l'admin et user ok, route principal une fois authentifié testé sur postman

--- src/main/java/com/chatopreview/api/ApiApplication.java
+++ src/main/java/com/chatopreview/api/ApiApplication.java
@@ -1,13 +1,35 @@
 package com.chatopreview.api;
 
+import com.chatopreview.api.api.entities.Role;
+import com.chatopreview.api.api.entities.User;
+import com.chatopreview.api.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
 
+	@Autowired
+	private UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	public void run(String... args){
+		User adminAccount = userRepository.findByRole(Role.ADMIN);
+		if(null == adminAccount){
+			User user = new User();
+
+
+	user.setEmail("admin@admin.com");
+		user.setName("admin");
+		user.setRole(Role.ADMIN);
+		user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+		userRepository.save(user);
+		}
 	}
 
 }
--- src/main/java/com/chatopreview/api/api/controllers/RentalsController.java
+++ src/main/java/com/chatopreview/api/api/controllers/RentalsController.java
@@ -21,24 +21,24 @@ public class RentalsController {
     }
 
 //@Autowired
-    @GetMapping("rental")
+    @GetMapping("auth/rental")
     public ResponseEntity<List<RentalDto>> getRentals() {
         return new ResponseEntity<>(rentalService.getAllRental(), HttpStatus.OK);
     }
 
-    @GetMapping("rental/{id}")
+    @GetMapping("auth/rental/{id}")
     public ResponseEntity<RentalDto> rentalDetail(@PathVariable int id) {
         return ResponseEntity.ok(rentalService.getRentalById(id));
     }
 
-    @PostMapping("rental/create")
+    @PostMapping("auth/rental/create")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<RentalDto> createRental(@RequestBody RentalDto rentalDto) {
         return new ResponseEntity<>(rentalService.createRental(rentalDto), HttpStatus.CREATED);
     }
 
 
-    @PutMapping("rental/{id}/update")
+    @PutMapping("auth/rental/{id}/update")
     public ResponseEntity<RentalDto> updateRental(@RequestBody RentalDto rentalDto, @PathVariable("id") int rentalId) {
         RentalDto response = rentalService.updateRental(rentalDto, rentalId);
         return new ResponseEntity<>(response, HttpStatus.OK);
